2774e86582da62be24b9ee32fbc7d2ef7f132fd2
Daniel Shechter <dshctr1993@gmail.com>
2022-07-13 19:51:26 +0300

    connected to scss

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+
 import "./styles/styles.css"
+import "./scss/main.scss"
+
 import ElementPlus from "element-plus"
 import "element-plus/dist/index.css"
 import VueGoogleMaps from "@fawmi/vue-google-maps"
+
+// const sass = require("sass")
+// const result = sass.compile(scssFilename)
 
 const app = createApp(App)
 
